6e60502cc206f35dadb9a50fba3d6a44e2e73733
yangyile <gogit@github.com/go-xlan/gogit>
2025-04-18 23:45:45 +0700

    改改打印结果的排版布局

--- w3mopenpage/w3m_open_page.go
+++ w3mopenpage/w3m_open_page.go
@@ -11,12 +11,12 @@ import (
 
 func Open(command *osexec.OsCommand, link string) {
 	output := rese.V1(command.Exec("w3m", "-dump", link))
-	zaplog.SUG.Debugln(string(output))
+	zaplog.SUG.Debug("[page]:", "\n", string(output), "\n", "----")
 }
 
 func Show(command *osexec.OsCommand, page string) {
 	output := rese.V1(command.ExecWith("w3m", []string{"-T", "text/html", "-dump"}, func(command *exec.Cmd) {
 		command.Stdin = strings.NewReader(page)
 	}))
-	zaplog.SUG.Debugln(string(output))
+	zaplog.SUG.Debug("[page]:", "\n", string(output), "\n", "----")
 }
